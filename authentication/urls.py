792744bded41807c0595c5d62aaf5e96bacec370
Alfred Warui <alfredwarui17@gmail.com>
2021-10-18 02:19:51 +0300

    create authentication views and their urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
   path('register/',views.register, name='register'),
-  path('profile/',views.profile,name='profile')
+  path('profile/',views.profile,name='profile'),
+  path('search_user/',views.search_results,name='search_results'),
+  path('follow/',views.follow,name='follow'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from . forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from app_activities.models import Image
+from app_activities.models import Image,Follow
 from .email import send_welcome_email
+from .models import Profile
+from django.contrib.auth.models import User
 
 #register view function
 def register(request): 
@@ -44,3 +46,58 @@ def profile(request):
     'p_form':p_form,
   }
   return render(request,'authentication/profile.html',context)
+
+def search_results(request):
+  
+  if 'username' in request.GET and request.GET["username"]:
+    search_term = request.GET.get("username")
+    found_user = User.objects.filter(username=search_term).first()
+    print(f'Found user:{found_user}')
+    message = f"{search_term}"
+    
+    current_user = search_term
+    logged_in_user = request.user.username
+    user_followers = len(Follow.objects.filter(user=current_user))
+    user_following = len(Follow.objects.filter(follower=current_user))
+    user_followers_list = Follow.objects.filter(user=current_user)
+    
+    user_followers_data = []
+    for i in user_followers_list: 
+      user_follower0 = i.follower
+      user_followers_data.append(user_follower0)
+      
+    if logged_in_user in user_followers_data: 
+      follow_button_value = 'unfollow'
+    else: 
+      follow_button_value = 'follow'
+      
+    posts=Image.objects.all()
+  
+    context = {
+      "posts": posts,
+      "current_user":current_user,
+      "user_followers":user_followers,
+      "user_following":user_following,
+      "follow_button_value":follow_button_value,
+      "message":message,
+      "users": found_user
+    }
+    
+    return render(request, 'search.html',context)
+
+  else:
+    message = "That user doesn't exist"
+    return render(request, 'search.html',{"message":message})
+  
+def follow(request): 
+  if request.method == 'POST': 
+    value = request.POST['value']
+    user = request.POST['user']
+    follower = request.POST['follower']
+    if value == 'follow': 
+      followers_count = Follow.objects.create(follower = follower, user = user)
+      followers_count.save()
+    else:
+      followers_count = Follow.objects.get(follower = follower, user = user)
+      followers_count.delete()
+    return redirect('search_results')
