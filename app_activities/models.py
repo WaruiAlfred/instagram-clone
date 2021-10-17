46258552ab588b728595ffbb3d0683144d408590
Alfred Warui <alfredwarui17@gmail.com>
2021-10-17 13:11:50 +0300

    delete user profile model

--- app_activities/models.py
+++ app_activities/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from authentication.models import Profile
 
-# Create your models here.
+#Profile model
+# class Profile(models.Model): 
+#   profile_photo = CloudinaryField('image')
+#   bio = models.TextField(blank=True)
+
+#   def save_profile(self): 
+#     '''function to save profile details'''
+#     self.save()
+    
+#   def update_profile(self): 
+#     '''Function to update profile'''
+#     Profile.objects.filter(id = self.id).update(bio = self.bio)
+#     self.save()
+    
+#   def delete_profile(self): 
+#     '''Function to delete a profile object'''
+#     profile = Profile.objects.get(id = self.id)
+#     if profile: 
+#       profile.delete()
+
+#   def __str__(self):
+#     return self.bio
+
+#Image model
+class Image(models.Model): 
+  image = CloudinaryField('image')
+  image_name = models.CharField(max_length=30)
+  image_caption = models.CharField(max_length=50)
+  date_added = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+  likes = models.IntegerField()
+  comments = models.CharField(max_length=100)
+  profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+  
+  def save_image(self): 
+    '''function to save an image and its details'''
+    self.save()
+    
+  def update_caption(self): 
+    '''Function to update caption'''
+    Image.objects.filter(id = self.id).update(image_caption = self.image_caption)
+    self.save()
+    
+  def delete_image(self): 
+    '''Function to delete an image object'''
+    image = Image.objects.get(id = self.id)
+    if image: 
+      image.delete()
+    
+  def __str__(self):
+    return self.image_name
